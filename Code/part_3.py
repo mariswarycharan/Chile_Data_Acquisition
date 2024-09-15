b3c44bbeefcafe858512bb9b72705c7c18120dbe
ChelappanKumar <chelappankumar23@gmail.com>
2024-09-15 18:13:30 +0530

    OrganismoPublico Mapping Added

--- Code/part_3.py
+++ Code/part_3.py
@@ -78,9 +78,59 @@ def sucursal_matching(dataframe,ini_forget_time):
     Rename_Pactivo_dict = {j.lower():i for i,j in market_basket_df.drop_duplicates("Rename Pactivo")[['Market or TA','Rename Pactivo']].to_numpy()}
     final_filtered_data['Market_or_TA'] = final_filtered_data['Pactivo'].str.lower().map(Rename_Pactivo_dict)
     
+    Organismo_Publico_mapping_file_path = 'Control/OrganismoPublico_Mapping.xlsx'
+    # Read the OrganismoPublico_Mapping and the main data file
+    organismo_mapping_df = pd.read_excel(Organismo_Publico_mapping_file_path)
+
+    # Perform the mapping by merging both dataframes on the 'OrganismoPublico' column
+    merged_df = pd.merge(final_filtered_data, organismo_mapping_df, on='OrganismoPublico', how='left')
+
+    # Replace the 'OrganismoPublico' column with the 'Mapped_Razon_Social_Cliente' column where available
+    merged_df['OrganismoPublico'] = merged_df['Mapped_Razon_Social_Cliente'].combine_first(merged_df['OrganismoPublico'])
+
+    # Drop the 'Mapped_Razon_Social_Cliente' column as it's no longer needed
+    final_df = merged_df.drop(columns=['Mapped_Razon_Social_Cliente'])
+
+    # Dictionary mapping specific values from "UnidadCompra" to replace in "OrganismoPublico"
+    replacement_dict = {
+        'COMANDO DE APOYO A LA FUERZA': 'Replacement Value 1',
+        'DIRECCION DE ABASTECIMIENTO DE LA ARMADA': 'Replacement Value 2',
+        'SERVICIO DE SALUD CHILOE': 'Replacement Value 3',
+        'FUERZA AEREA DE CHILE COMANDO LOGISTICO': 'Replacement Value 4'
+    }
+
+    # Replacing values in "OrganismoPublico" based only on the specific "UnidadCompra" values in the dictionary
+    final_df['OrganismoPublico'] = final_df.apply(
+        lambda row: replacement_dict.get(row['UnidadCompra'], row['OrganismoPublico']),
+        axis=1
+    )
+
+
+    sector_df = pd.read_excel('Control/Sector.xlsx')
+
+    # Create a mapping dictionary from the Excel file
+    mapping_dict = sector_df.set_index('Sector')['Maped_Instituciones'].to_dict()
+    # Map the 'sector' column to the 'Maped_Instituciones' values
+    final_df['Instituciones'] = final_df['sector'].map(mapping_dict)
+
+    target_phrase = 'Cenabast'
+
+    # Replacement logic based on the presence of the specific phrase
+    final_df['Instituciones'] = final_df.apply(
+        lambda row: 'Cenabast' if target_phrase in row['OrganismoPublico'] else row['Instituciones'],
+        axis=1)
+    final_df['cantidad'] = final_df['cantidad'].astype(int)
+    final_df['precioNeto'] = final_df['precioNeto'].astype(int)
+    final_df['totalLineaNeto'] = final_df['totalLineaNeto'].astype(int)
+
+    # Format the columns with a comma separator and 0 decimal places
+    final_df['cantidad'] = final_df['cantidad'].apply(lambda x: "{:,}".format(x))
+    final_df['precioNeto'] = final_df['precioNeto'].apply(lambda x: "{:,}".format(x))
+    final_df['totalLineaNeto'] = final_df['totalLineaNeto'].apply(lambda x: "{:,}".format(x))
     
-    final_filtered_data = final_filtered_data[["Codigo", "Link", "EspecificacionComprador", "EspecificacionProveedor", "OrganismoPublico", "Sucursal_Proveedor", "Pactivo", "Brand", "Presentación", "cantidad", "precioNeto", "totalLineaNeto", "FechaEnvio", "Mes", "Month", "Year", "Market_or_TA", "RutUnidadCompra", "CiudadUnidadCompra", "RutSucursal", "sector", "RegionUnidadCompra", "Tipo", "CodigoLicitacion", "CorporacionesPHT"]
+    
+    final_df = final_df[["Codigo", "Link", "EspecificacionComprador", "EspecificacionProveedor", "OrganismoPublico", "Sucursal_Proveedor", "Pactivo", "Brand", "Presentación", "cantidad", "precioNeto", "totalLineaNeto", "FechaEnvio", "Mes", "Month", "Year", "Market_or_TA", "RutUnidadCompra", "CiudadUnidadCompra", "RutSucursal", "sector","Instituciones", "RegionUnidadCompra", "Tipo", "CodigoLicitacion", "CorporacionesPHT"]
 ]
     
-    return final_filtered_data
+    return final_df
 
